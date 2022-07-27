f653c4c862fd4773a239593da0b2609f5c2a66d1
Monica Daros <monica.daros@warren.com.br>
2022-07-27 11:58:35 -0300

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:warrenapi/screens/wallet/wallet.dart';
 import 'package:warrenapi/widgets/bottom_bar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'l10n/l10n.dart';
 
 void main() {
   runApp(
@@ -17,6 +21,13 @@ void main() {
         theme: ThemeData(
           primarySwatch: Colors.pink,
         ),
+        supportedLocales: L10n.all,
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
       ),
     ),
   );
